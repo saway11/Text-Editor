9d725e219a72cb2e268033f7c30fefc3fe160200
saway11 <rgarcia20.m@gmail.com>
2023-07-11 16:32:17 -0700

    added logic for a method that gets all the content from the databse

--- client/src/js/databse.js
+++ client/src/js/databse.js
@@ -12,12 +12,23 @@ const initdb = async () =>
         },
     });
 
-    // Add logic to a method that accepts some content and adds it to the database
-    export const putDb = async (content) => {
-        const contentDb = await openDB('jate', '1');
-        const tx = contentDb.transaction('jate', 'readwrite');
-        const store = tx.objectStore('jate');
-        const request = await store.put({ id: 'content', content });
-        const result = await request;
-        console.log('🚀 - data saved to the database', result);
-    };
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+    const contentDb = await openDB('jate', 1);
+    const tx = contentDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = await store.put({ id: 'content', content });
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+    const contentDb = await openDB('jate', 1);
+    const tx = contentDb.transaction('jate', 'readonly');
+    const store = tex.objectStore('jate');
+    const result = await store.get('content');
+    console.log('getDb', result);
+    return result? result.content : '';
+};
+initdb();
