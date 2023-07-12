c53e364d30a9bffac00a042470d715bc06824b04
saway11 <rgarcia20.m@gmail.com>
2023-07-11 18:03:04 -0700

    implenment a click event handler, show prompt

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,10 +4,23 @@ const butInstall = document.getElementById('buttonInstall');
 // Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     // Store the triggered events
-    window.deferredPrompts = event;
+    window.deferredPrompt = event;
 
     // Remove the hidden class from the button
     butInstall.classList.toggle('hidden', false);
 });
 
 // Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    // Show prompt
+    promptEvent.prompt();
+
+    // Reset the deferred prompt variable, it can only be used once.
+    
+})
