ee277e42fd508306e15ce3b949acbbc8c1013676
saway11 <rgarcia20.m@gmail.com>
2023-07-11 18:00:49 -0700

    remove the hidden class from the bottom

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,5 +6,8 @@ window.addEventListener('beforeinstallprompt', (event) => {
     // Store the triggered events
     window.deferredPrompts = event;
 
-    
-})
+    // Remove the hidden class from the button
+    butInstall.classList.toggle('hidden', false);
+});
+
+// Implement a click event handler on the `butInstall` element
