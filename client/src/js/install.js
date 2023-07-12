d2ed624a3da6f389ac27928cfd7bd50d65cce37c
saway11 <rgarcia20.m@gmail.com>
2023-07-11 17:59:40 -0700

    add an event handler to the beforeinstall prompt

---
+++ client/src/js/install.js
@@ -0,0 +1,10 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Store the triggered events
+    window.deferredPrompts = event;
+
+    
+})
