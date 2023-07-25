fd4585b6866681e7fd45b9c11670c1a13a484389
saway11 <rgarcia20.m@gmail.com>
2023-07-25 12:14:25 -0700

    added starter code

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Text-Editor
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+# Description
+A Text Editor application that runs in the browser and alows its user to keep editing the text. The user also has the ability to save text for future reference, and can download this application as an icon on the users desktop. The application will function offline.
+# Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [URL](#URL)
+- [Screenshot](#screenshot)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+# Installation
+To run this application on your local machine, follow these steps:
+1. Download the code in your local machine and run `npm install` in the terminal to install all the dependencies.
+2. Start the server by running `npm run start:dev` in command prompt.
+3. Run the application in the browser by making a request to http://localhost:3000/.
+# Usage
+When you open the application in the browser, the user can view the default text. The user
+can start adding text and it will save the data as is. When you revisit you can see the 
+text you added before. You can also download it on to your desktop by clicking install 
+button. This is a single-page progressive web application. Text Editor features a number 
+of data persistence techniques (IndexDB and Local Storage) that serve as redundancy
+in case one of the options is not supported by the browser.
+# URL
+# Screenshot
+# Credits
+UC Berkeley Extension Full Stack Coding program
+Tutor
+ChatGBT
+Google
+# License
+This project is licensed under the MIT license. To learn more please visit https://choosealicense.com/licenses/mit.
+# Questions
+View my code and other projects at saway11.
+If you have any questions please email me at rgarcia20.@gmail.com.
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,65 +6,25 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-
 module.exports = () => {
-    return {
-        mode: 'development',
-        entry: {
-            main: './src/js/index.js',
-            install: './src/js/install.js',
-        },
-        output: {
-            filename: '[name]'.bundle.js,
-            path: path.resolve(__dirname, 'dist'),
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './index.html',
-                title: 'Progressive Web Applicaiton',
-            }),
-            new InjectManifest({
-                swSrc: './src-sw.js',
-                swDest: 'src-sw.js',
-            }),
-            new WebpackPwaManifest({
-                fingerprint: false,
-                inject: true,
-                name: 'Progressive Web Application',
-                short_name: 'PWA',
-                description: 'A simple PWA application',
-                background_color: '#ffffff',
-                theme_color: '#000000',
-                start_url: './',
-                publicPath: './',
-                incons: [
-                    {
-                        src: path.resolve('src/images/logo.png'),
-                        sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-                        destination: path.join('assets', 'icons'),
-                    }
-                ],
-            }),
-        ],
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      
+    ],
 
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: ['style-loader', 'css-loader'],
-                },
-                {
-                    test: /\.m?js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env'],
-                            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-                        },
-                    },
-                },
-            ],
-        },
-    };
+    module: {
+      rules: [
+        
+      ],
+    },
+  };
 };
